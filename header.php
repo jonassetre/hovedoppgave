7c7414ecdaea0b54f24d766f32e67d6691381759
Maryna <mge013@post.uit.no>
2021-02-25 23:13:17 +0100

    front-end for index.php, create subject(popup form)

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Header</title>
+    <link href="stylesheets/index.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<div class="header">
+    <div class="headerLogo">
+        <img src="media/UiT_Logo_Bok_2l_Bla_RGB.png" alt="Logo">
+    </div>
+    <div class="headerOptions">
+        <li><a href="logout.php">Logout</a></li>
+        <li><a href="profile.php">Profile</a></li>
+    </div>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,2 +1,21 @@
+<?php require_once 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Header</title>
+    <script src="./frontend/index.js"></script>
+</head>
 
+<body>
+<div class="container">
 
+    <img src="media/mango-matter-1Ms3Zsb1v7g-unsplash.jpg" alt="Home">
+    <h1>Velkommen til UiTs spørsmålsbank</h1>
+
+    <a class="example_b" onclick="popUpNewSubject()"> + Nytt emne</a>
+    <?php include('./frontend/popupform.html'); ?>
+</div>
+</body>
+</html>
