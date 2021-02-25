eadc4197dc6129a477d689e7ac5ba3d36fcb3ab7
Jonas Sætre <jonassetre@gmail.com>
2021-02-25 16:25:00 +0100

    Updated App.php
    App class setup

--- src/App.php
+++ src/App.php
@@ -1,15 +1,34 @@
 <?php
+
+include_once "model/test.php";
+
 require_once '../backend/connect.php';
 
-function insertSubject($subjectCode, $subjectTitle){
-    $query = $db->prepare(
-        'INSERT INTO Subject (subjectCode, subjectTitle)
+class App
+{
+    static $db;
+    
+    public function __construct(PDO $db) {
+        self::$db = $db;
+    }
+
+    public static function prepare(string $statement) : PDOStatement {
+        return self::$db->prepare($statement);
+    }
+
+    function insertSubject($subjectCode, $subjectTitle){
+        $query = $db->prepare(
+            'INSERT INTO Subject (subjectCode, subjectTitle)
          VALUES (:subjectCode, :subjectTitle);'
-    );
-    $query->bindParam(':subjectCode', $subjectCode, PDO::PARAM_STR);
-    $query->bindParam(':subjectTitle', $subjectTitle, PDO::PARAM_STR);
-    $query->execute();
-    return 0;
+        );
+        $query->bindParam(':subjectCode', $subjectCode, PDO::PARAM_STR);
+        $query->bindParam(':subjectTitle', $subjectTitle, PDO::PARAM_STR);
+        $query->execute();
+        return 0;
+    }
+
 }
+
+
 
 
