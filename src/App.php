172150a30f0ec32269f40947a0eddabdbc4ee091
Jonas Sætre <jonassetre@gmail.com>
2021-02-26 16:48:08 +0100

    Update App.php
    Added static variable to functions.
    Updated what the functions return.
    + small fixes

--- src/App.php
+++ src/App.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once "model/test.php";
+include_once "model/Answer.php";
 
 
 require_once '../backend/connect.php';
@@ -18,24 +18,26 @@ class App
     }
 
     function insertSubject($subjectCode, $subjectTitle){
-        $query = $db->prepare(
-            'INSERT INTO Subject (subjectCode, subjectTitle)
+        try {
+            $stmt = self::prepare(
+                'INSERT INTO Subject (subjectCode, subjectTitle)
          VALUES (:subjectCode, :subjectTitle);'
-        );
-        $query->bindParam(':subjectCode', $subjectCode, PDO::PARAM_STR);
-        $query->bindParam(':subjectTitle', $subjectTitle, PDO::PARAM_STR);
-        $query->execute();
-        return 0;
+            );
+            $stmt->bindParam(':subjectCode', $subjectCode, PDO::PARAM_STR);
+            $stmt->bindParam(':subjectTitle', $subjectTitle, PDO::PARAM_STR);
+            return $stmt->execute();
+        } catch  (Exception $e) {
+            print $e->getMessage(). PHP_EOL;
+        }
     }
 
     public function createGroup($groupName, $id_subject): Group{
         try {
-            $stmt = $db->prepare("INSERT INTO `Group`(`groupName`, `Subject_idSubject`)" .
+            $stmt = self::prepare("INSERT INTO `Group`(`groupName`, `Subject_idSubject`)" .
                 "VALUES (:groupName, :id_subject)");
             $stmt->bindParam(":groupName", $groupName, PDO::PARAM_STR);
             $stmt->bindParam(":id_subject", $id_subject, PDO::PARAM_INT);
-            $stmt->execute();
-            return 0;
+            return $stmt->execute();
         } catch (Exception $e) {
             print  $e->getMessage(). PHP_EOL;
         }
