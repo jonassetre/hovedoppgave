b44c56ce22cc822b056b947079709c612b1bf565
Margarita Emelianova <mem004@post.uit.no>
2021-03-01 10:37:51 +0100

    frontend create_group.php

---
+++ frontend/subject.js
@@ -0,0 +1,12 @@
+function popUpNewGroup()
+{
+    document.getElementById('id01').style.display='block'
+    let modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+}
