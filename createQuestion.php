6e64920c12404f44e22654b07ea7d6d7224ee778
fvu000 <vullum.fredrik@gmail.com>
2021-03-12 15:19:43 +0100

    more fixes to front end of createQuestion.php to get needed variables for back end

--- createQuestion.php
+++ createQuestion.php
@@ -32,7 +32,7 @@ if (isset($_POST['btnSaveQuestion'])){
     $tag = $_POST['inputQuestionTagger'];
     $score = $_POST['inputQuestionPoeng'];
     $Group_idGroup = $_GET['group'];
-    $questContent = 'this is a test';
+    $questContent = $_POST['inputQuestionContent'];
 
     if( $app->createQuestion($questContent, $diffDegree, $tag, $score, $Group_idGroup)){
         $app->redirect('Nytt spørsmål ble lagt til.', 'subject.php?course=' . $data['Subject_idSubject']);
@@ -45,121 +45,127 @@ if (isset($_POST['btnSaveQuestion'])){
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Header</title>
-    <script src="frontend/index.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
-          integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Header</title>
+        <script src="frontend/index.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+              integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                crossorigin="anonymous"></script>
+    </head>
 
-<body>
-    <form action="" name="createQuestionForm" id="createQuestionFormId" method="post">
-        <div class="container">
-            <div class="question-container">
-                <div class="questionTop">
-                    <a class="backToGroup" onclick="window.location.href='subject.php';"> Tilbake til grupper</a>
-                </div>
+    <body>
+        <form action="" name="createQuestionForm" id="createQuestionFormId" onsubmit="getTextEditorText()" method="post">
+            <div class="container">
+                <div class="question-container">
+                    <div class="questionTop">
+                        <a class="backToGroup" onclick="window.location.href='subject.php';"> Tilbake til grupper</a>
+                    </div>
 
-                <div class="questionDown">
-                    <div class="form-p1">
-                        <label>
-                            <input class="inputQuestionGroupName" type="text" name="inputQuestionGroupName" value="<?php echo $data['groupName'] ?>" >
-                        </label>
+                    <div class="questionDown">
+                        <div class="form-p1">
+                            <label>
+                                <input class="inputQuestionGroupName" type="text" name="inputQuestionGroupName" value="<?php echo $data['groupName'] ?>" >
+                            </label>
 
-                        <select id="dropdown2" name="inputQuestionDifficulty" size="1">
-                            <option value="Enkel">Enkel</option>
-                            <option value="Middels">Middels</option>
-                            <option value="Høy">Høy</option>
-                        </select>
+                            <select id="dropdown2" name="inputQuestionDifficulty" size="1">
+                                <option value="Enkel">Enkel</option>
+                                <option value="Middels">Middels</option>
+                                <option value="Høy">Høy</option>
+                            </select>
 
-                        <label>
-                            <input class="inputQuestionPoeng" type="text" name="inputQuestionPoeng" required>
-                            <span>poeng</span>
-                        </label>
+                            <label>
+                                <input class="inputQuestionPoeng" type="text" name="inputQuestionPoeng" required>
+                                <span>poeng</span>
+                            </label>
 
-                        <div class="form-p2">
-                            <div id="editor" name="textEditor" contenteditable="false">
-                                <section id="toolbar" name="toolbar">
-                                    <div id="bold" class="icon fas fa-bold"></div>
-                                    <div id="italic" class="icon fas fa-italic"></div>
-                                    <div id="underline" class="icon fas fa-underline"></div>
-                                    <div id="insertUnorderedList" class="icon fas fa-list-ul"></div>
-                                    <div id="insertOrderedList" class="icon fas fa-list-ol"></div>
-                                    <div id="justifyLeft" class="icon fas fa-align-left"></div>
-                                    <div id="justifyRight" class="icon fas fa-align-right"></div>
-                                    <div id="justifyCenter" class="icon fas fa-align-center"></div>
-                                    <div id="justifyFull" class="icon fas fa-align-justify"></div>
-
-                                </section>
-                                <section id="page" name="inputQuestionContent" contenteditable="true">
-                                </section>
-                            </div>
-                            <script src="frontend/script.js"></script>
-                        </div>
-                        <label>
-                            <h4>#tagger <input class="inputQuestionTagger" type="text" name="inputQuestionTagger" required placeholder="Skriv minst et nøkkelord">
-                            </h4>
-                        </label>
-                        <div class="form-p3">
-                            <h4>Svar:</h4>
-                            <div id="editor">
-                                <div id="correctAnswerForm">
-                                    <div class="correctAnswer">
-                                        <label for="name">Riktig svar</label>
-                                        <input class="inputQuestion" type="text" name="inputQuestion" placeholder="">
-
-                                        <label for="name">Kommentar </label>
-                                        <input class="inputComment" type="text" name="inputComment" placeholder="">
-                                    </div>
-                                </div>
-                                <div id="wrongAnswerForm">
-                                    <div class="wrongAnswer">
-                                        <label for="name">Mulig svar</label>
-                                        <input class="inputQuestion" type="text" name="inputQuestion" placeholder="">
-
-                                        <label for="name">Kommentar</label>
-                                        <input class="inputComment" type="text" name="inputComment" placeholder="">
-                                    </div>
-                                </div>
-
-                                <div class="generellComment">
-                                    <label for="name">Generelle kommentarer til
-                                        testen</label>
-                                    <input class="inputGenerellComment" type="text" name="inputGenerellComment"
-                                           placeholder="">
-                                </div>
-                                <div>
-
-                                    <section id="page">
-                                        <button id="btnCorrect" type="button" onclick="newCorrectAnswer()" name="btnCorrect">+
-                                            Riktig svar
-                                        </button>
-                                        <button id="btnFalse" type="button" name="btnFalse"
-                                                onclick="newWrongAnswer()">+ Mulig svar
-                                        </button>
+                            <div class="form-p2">
+                                <div id="editor" name="textEditor" contenteditable="false">
+                                    <section id="toolbar" name="toolbar">
+                                        <div id="bold" class="icon fas fa-bold"></div>
+                                        <div id="italic" class="icon fas fa-italic"></div>
+                                        <div id="underline" class="icon fas fa-underline"></div>
+                                        <div id="insertUnorderedList" class="icon fas fa-list-ul"></div>
+                                        <div id="insertOrderedList" class="icon fas fa-list-ol"></div>
+                                        <div id="justifyLeft" class="icon fas fa-align-left"></div>
+                                        <div id="justifyRight" class="icon fas fa-align-right"></div>
+                                        <div id="justifyCenter" class="icon fas fa-align-center"></div>
+                                        <div id="justifyFull" class="icon fas fa-align-justify"></div>
+                                    </section>
+                                        <section id="page" contenteditable="true">
                                     </section>
                                 </div>
+                                <input type="hidden" name="inputQuestionContent" id="inputQuestionContentId">
+                                <script src="frontend/script.js"></script>
                             </div>
+                            <label>
+                                <h4>#tagger <input class="inputQuestionTagger" type="text" name="inputQuestionTagger" required placeholder="Skriv minst et nøkkelord">
+                                </h4>
+                            </label>
+                            <div class="form-p3">
+                                <h4>Svar:</h4>
+                                <div id="editor">
+                                    <div id="correctAnswerForm">
+                                        <div class="correctAnswer">
+                                            <label for="name">Riktig svar</label>
+                                            <input class="inputQuestion" type="text" name="inputQuestion" placeholder="">
 
-                            <button id="btnSaveQuestion" type="submit" name="btnSaveQuestion">Lagre</button>
-                            <button id="btnCancel" type="button" name="button2"
-                                    onclick="window.location.href='subject.php';"
-                                    class="cancelbtn">Avbryt
-                            </button>
+                                            <label for="name">Kommentar </label>
+                                            <input class="inputComment" type="text" name="inputComment" placeholder="">
+                                        </div>
+                                    </div>
+                                    <div id="wrongAnswerForm">
+                                        <div class="wrongAnswer">
+                                            <label for="name">Mulig svar</label>
+                                            <input class="inputQuestion" type="text" name="inputQuestion" placeholder="">
+
+                                            <label for="name">Kommentar</label>
+                                            <input class="inputComment" type="text" name="inputComment" placeholder="">
+                                        </div>
+                                    </div>
+
+                                    <div class="generellComment">
+                                        <label for="name">Generelle kommentarer til
+                                            testen</label>
+                                        <input class="inputGenerellComment" type="text" name="inputGenerellComment"
+                                               placeholder="">
+                                    </div>
+                                    <div>
+
+                                        <section id="page">
+                                            <button id="btnCorrect" type="button" onclick="newCorrectAnswer()" name="btnCorrect">+
+                                                Riktig svar
+                                            </button>
+                                            <button id="btnFalse" type="button" name="btnFalse"
+                                                    onclick="newWrongAnswer()">+ Mulig svar
+                                            </button>
+                                        </section>
+                                    </div>
+                                </div>
+
+                                <button id="btnSaveQuestion" type="submit" name="btnSaveQuestion">Lagre</button>
+                                <button id="btnCancel" type="button" name="button2"
+                                        onclick="window.location.href='subject.php';"
+                                        class="cancelbtn">Avbryt
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    <?php require_once 'footer.php'; ?>
-    <script src="frontend/jquery.js"></script>
-    <script src="frontend/app.js"></script>
-    </form>
-</body>
-
+        <?php require_once 'footer.php'; ?>
+        <script src="frontend/jquery.js"></script>
+        <script src="frontend/app.js"></script>
+        </form>
+        <script>
+            function getTextEditorText() {
+                var content = document.getElementById('page').innerHTML;
+                document.getElementById('inputQuestionContentId').value = content;
+            }
+        </script>
+        <script src="frontend/script.js"></script>
+    </body>
 </html>
